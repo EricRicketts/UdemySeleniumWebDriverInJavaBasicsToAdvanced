99076682c5e5199737dbea276f2953138df928f2
Eric Ricketts <eric_ricketts@me.com>
2022-10-29 08:10:20 -0400

    first commit got web browser to work

---
+++ Section2/class-introduction/src/test/java/com/example/classintroduction/SelIntroduction.java
@@ -0,0 +1,13 @@
+package com.example.classintroduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelIntroduction {
+
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://rahulshettyacademy.com");
+  }
+}
