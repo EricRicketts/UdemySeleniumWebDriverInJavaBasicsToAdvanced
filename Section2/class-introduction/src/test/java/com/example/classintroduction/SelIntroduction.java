b619c7d7eb38222747059f1891f38c8302ac3e4c
Eric Ricketts <eric_ricketts@me.com>
2022-10-30 14:50:46 -0400

    have all four webdriver working on mac: chrome, firefox, edge, and safari

--- Section2/class-introduction/src/test/java/com/example/classintroduction/SelIntroduction.java
+++ Section2/class-introduction/src/test/java/com/example/classintroduction/SelIntroduction.java
@@ -2,12 +2,21 @@ package com.example.classintroduction;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class SelIntroduction {
 
   public static void main(String[] args) {
     System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+//    System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+//    System.setProperty("webdriver.edge.driver", "/usr/local/bin/msedgedriver");
+//    System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
     WebDriver driver = new ChromeDriver();
+//    WebDriver driver = new FirefoxDriver();
+//    WebDriver driver = new EdgeDriver();
+//    WebDriver driver = new SafariDriver();
     driver.get("https://rahulshettyacademy.com");
   }
 }
