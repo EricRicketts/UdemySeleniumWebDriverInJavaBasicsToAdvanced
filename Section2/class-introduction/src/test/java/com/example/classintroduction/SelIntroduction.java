cf03e177c6e5ba025ca3c19471aa557a3b3d5990
EricRicketts <eric_ricketts@me.com>
2022-10-31 17:04:42 -0400

    get windows drivers to work, Safari excluded

--- Section2/class-introduction/src/test/java/com/example/classintroduction/SelIntroduction.java
+++ Section2/class-introduction/src/test/java/com/example/classintroduction/SelIntroduction.java
@@ -9,14 +9,25 @@ import org.openqa.selenium.safari.SafariDriver;
 public class SelIntroduction {
 
   public static void main(String[] args) {
-    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+// Windows webdriver
+// Apple stopped supporting Safari on Windows after version 5.17 Safari is now on version 10, it is not worth the
+// effort
+//    System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\WebDrivers\\chromedriver.exe");
+//    System.setProperty("webdriver.edge.driver", "C:\\Program Files\\WebDrivers\\msedgedriver.exe");
+    System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\WebDrivers\\geckodriver.exe");
+
+
+//    Mac webdrivers
+//    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 //    System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
 //    System.setProperty("webdriver.edge.driver", "/usr/local/bin/msedgedriver");
 //    System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
-    WebDriver driver = new ChromeDriver();
-//    WebDriver driver = new FirefoxDriver();
+
+//    WebDriver driver = new ChromeDriver();
+    WebDriver driver = new FirefoxDriver();
 //    WebDriver driver = new EdgeDriver();
 //    WebDriver driver = new SafariDriver();
     driver.get("https://rahulshettyacademy.com");
+    driver.close();
   }
 }
