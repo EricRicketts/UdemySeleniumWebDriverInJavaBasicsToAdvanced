582fb986175badcc5c04b769fad1cec12ee36736
EricRicketts <eric_ricketts@me.com>
2022-11-01 19:33:13 -0400

    loops and conditionals

--- Section3/core-java-basics/src/test/java/org/example/AppTest.java
+++ Section3/core-java-basics/src/test/java/org/example/AppTest.java
@@ -3,6 +3,8 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -32,6 +34,75 @@ public class AppTest {
     String y = "Foo Bars";
     String expected = "5 Foo Bars";
     String results = x + " " + y;
+    assertEquals(expected, results);
+  }
+
+  @Test
+  @DisplayName("arrays can hold more than one value")
+  public void testArraysCanHoldMoreThanOneValue() {
+    int[] ary;
+    ary = new int[] {0, 1, 2, 3, 4};
+    int[] expected = {0, 1, 2, 3, 4};
+    assertArrayEquals(expected, ary);
+  }
+
+  @Test
+  @DisplayName("iterate through an array")
+  public void testIterateThroughAnArray() {
+    int[] expected = {0, 1, 2, 3, 4};
+    int[] results = new int[5];
+    for (int i = 0; i < expected.length; i++) {
+      results[i] = i;
+    }
+
+    assertArrayEquals(expected, results);
+  }
+
+  @Test
+  @DisplayName("iterate through array with enhanced for loop")
+  public void testEnhancedForLoop() {
+    String[] expectedAry = {"foo", "bar", "fizz", "buzz"};
+    String results = "";
+    for (String s: expectedAry) {
+      results += " " + s;
+    }
+    results = results.stripLeading();
+    String expected = String.join(" ", expectedAry);
+
+    assertEquals(expected, results);
+  }
+
+  @Test
+  @DisplayName("iterate over an array with a conditional")
+  public void testIterateOverArrayWithConditional() {
+    String[] expectedAry = {"foo", "bar", "fizz", "buzz"};
+    String results = "";
+    for (int i = 0; i < expectedAry.length; i++) {
+      if (i % 2 == 0) {
+        results += " " + expectedAry[i];
+      }
+    }
+    results = results.stripLeading();
+    assertEquals("foo fizz", results);
+  }
+
+  @Test
+  @DisplayName("conditionals")
+  public void testConditionals() {
+    String[] expectedAry = {"foo", "bar", "fizz", "buzz"};
+    String results = "";
+    for (String s: expectedAry) {
+      if (s.equals("foo")) {
+        results += s;
+      } else if (s.equals("bar")) {
+        results += " " + s;
+      } else if (s.equals("fizz")) {
+        results += " " + s;
+      } else {
+        results += " " + s;
+      }
+    }
+    String expected = String.join(" ", expectedAry);
     assertEquals(expected, results);
   }
 }
