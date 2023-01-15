e6b6283b07bf6fba1732d942422aae4332a670b6
EricRicketts <eric_ricketts@me.com>
2023-01-15 12:52:12 -0500

    first basic selenium commands

---
+++ Section2/web-driver-methods/src/test/java/com/example/webdrivermethods/WebDriverMethods.java
@@ -0,0 +1,13 @@
+package com.example.webdrivermethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverMethods {
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\WebDrivers\\chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://rahulshettyacademy.com");
+    driver.close();
+  }
+}
