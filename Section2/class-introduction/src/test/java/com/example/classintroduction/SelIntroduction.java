71cfc98b8514bcc8d262e0867e75e49c15294063
EricRicketts <eric_ricketts@me.com>
2023-01-15 12:21:21 -0500

    get introductory projects working on windows

--- Section2/class-introduction/src/test/java/com/example/classintroduction/SelIntroduction.java
+++ Section2/class-introduction/src/test/java/com/example/classintroduction/SelIntroduction.java
@@ -13,13 +13,13 @@ public class SelIntroduction {
 // Windows webdriver
 // Apple stopped supporting Safari on Windows after version 5.17 Safari is now on version 10, it is not worth the
 // effort
-//    System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\WebDrivers\\chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\WebDrivers\\chromedriver.exe");
 //    System.setProperty("webdriver.edge.driver", "C:\\Program Files\\WebDrivers\\msedgedriver.exe");
 //    System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\WebDrivers\\geckodriver.exe");
 
 
 //    Mac webdrivers
-    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+//    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 //    System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
 //    System.setProperty("webdriver.edge.driver", "/usr/local/bin/msedgedriver");
 //    System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
