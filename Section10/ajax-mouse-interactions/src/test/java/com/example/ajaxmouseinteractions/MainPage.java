1a65ba34a9e8c126534a76e5b933c6526b2d4386
Eric Ricketts <eric_ricketts@me.com>
2023-03-19 09:11:39 -0400

    verify sign in pop up on amazon website

--- Section10/ajax-mouse-interactions/src/test/java/com/example/ajaxmouseinteractions/MainPage.java
+++ Section10/ajax-mouse-interactions/src/test/java/com/example/ajaxmouseinteractions/MainPage.java
@@ -3,12 +3,15 @@ package com.example.ajaxmouseinteractions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
-// page_url = https://www.jetbrains.com/
 public class MainPage {
-  @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
-  public WebElement seeDeveloperToolsButton;
+  @FindBy(how = How.XPATH, using = "//span[contains(text(),'Account & Lists')]")
+  public WebElement accountsAndListsElement;
+
+  @FindBy(how = How.CSS, using = "div#nav-flyout-ya-signin > a > span")
+  public WebElement popUpSignInButton;
 
   public MainPage(WebDriver driver) {
     PageFactory.initElements(driver, this);
--- Section10/ajax-mouse-interactions/src/test/java/com/example/ajaxmouseinteractions/MainPageTest.java
+++ Section10/ajax-mouse-interactions/src/test/java/com/example/ajaxmouseinteractions/MainPageTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.*;
 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -22,11 +25,12 @@ public class MainPageTest {
 
   @BeforeEach
   public void setUp() {
+    final String url = "https://www.amazon.com";
     int implicitWaitTime = 10;
     driver = new ChromeDriver();
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitTime));
-    driver.get("https://www.jetbrains.com/");
+    driver.get(url);
 
     mainPage = new MainPage(driver);
   }
@@ -41,5 +45,17 @@ public class MainPageTest {
     int explicitWaitTime = 10;
     Duration duration = Duration.ofSeconds(explicitWaitTime);
     WebDriverWait wait = new WebDriverWait(driver, duration);
+    Actions actions = new Actions(driver);
+
+    WebElement accountAndListsElement = wait.until(
+        ExpectedConditions.visibilityOf(mainPage.accountsAndListsElement)
+    );
+    Assertions.assertNotNull(accountAndListsElement);
+    actions.moveToElement(accountAndListsElement).build().perform();
+
+    WebElement popUpSignInButton = wait.until(
+        ExpectedConditions.visibilityOf(mainPage.popUpSignInButton)
+    );
+    Assertions.assertNotNull(popUpSignInButton);
   }
 }
