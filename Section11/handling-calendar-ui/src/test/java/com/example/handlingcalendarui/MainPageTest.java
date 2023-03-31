997e1e17ff46f7383177f1c1edcf05ad0ab5fab1
Eric Ricketts <eric_ricketts@me.com>
2023-03-31 08:52:02 -0400

    snapshot using custom ExpectedCondition to return true when element is in viewport.  This will be moved to a utility class

--- Section11/handling-calendar-ui/src/test/java/com/example/handlingcalendarui/MainPageTest.java
+++ Section11/handling-calendar-ui/src/test/java/com/example/handlingcalendarui/MainPageTest.java
@@ -7,17 +7,41 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
+import javax.annotation.Nullable;
 import java.time.Duration;
-import java.util.Objects;
 
 public class MainPageTest {
   private WebDriver driver;
   private MainPage mainPage;
 
+  private static ExpectedCondition<Boolean> isVisibleInViewport(WebElement element) {
+    return new ExpectedCondition<>() {
+      @Nullable
+      @Override
+      public Boolean apply(@Nullable WebDriver input) {
+        if (input != null) {
+          return (Boolean) ((JavascriptExecutor) input).executeScript(
+              "var element = arguments[0]; " +
+                  "var rect = element.getBoundingClientRect(); " +
+                  "return ( " +
+                  "rect.top >= 0 && " +
+                  "rect.left >= 0 && " +
+                  "rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && " +
+                  "rect.right <= (window.innerWidth || document.documentElement.clientWidth) " +
+                  ");"
+              , element);
+        } else {
+          return false;
+        }
+      }
+    };
+  }
   @BeforeAll
   public static void oneTimeSetup() {
     System.setProperty("webdriver.http.factory", "jdk-http-client");
@@ -55,9 +79,6 @@ public class MainPageTest {
     );
     Assertions.assertNotNull(socialMediaIconsDivElement);
 
-    // scroll the input to the top of the page
-    ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView()", mainPage.travelDateInput);
-    Thread.sleep(1000);
 
     // get the current date
     String currentDateString = String.valueOf(java.time.LocalDate.now());
@@ -65,13 +86,29 @@ public class MainPageTest {
     String[] yearMonthDay = currentDateString.split("-");
     String currentDay = yearMonthDay[2];
 
-    // click on the calendar input to reveal the calendar
+
+    // ensure the travel date input field is visible
+    WebElement travelDateInput = wait.until(
+        ExpectedConditions.visibilityOf(mainPage.travelDateInput)
+    );
+    // scroll the travel date input to the top of the page it is visible within the viewport
+    ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView()", travelDateInput);
+
+    // got this function from
+    // https://medium.com/bliblidotcom-techblog/how-to-create-custom-expectedcondition-to-check-whether-an-element-within-web-viewport-not-on-fad42bc4d0f9
+    // the code below works but I really should put it in a separate class like the URL suggested
+    boolean isVisibleInViewport = wait.until(isVisibleInViewport(travelDateInput));
+    Assertions.assertTrue(isVisibleInViewport);
+
+    // move the mouse to the center of the travel date input and then click
+    new Actions(driver).moveToElement(travelDateInput).build().perform();
+    mainPage.travelDateInput.click();
+
     // cycle through all the days viewed in the calendar until you come across
     // the desired day of the month
-    mainPage.travelDateInput.click();
     for (int index = 0; index < mainPage.allDatesForTravel.size(); index++) {
       currentDayOfMonth = mainPage.allDatesForTravel.get(index);
-      if (Objects.equals(currentDayOfMonth.getText(), currentDay)) {
+      if (currentDayOfMonth.getText().equalsIgnoreCase(currentDay)) {
         currentDayOfMonth.click();
         break;
       }
