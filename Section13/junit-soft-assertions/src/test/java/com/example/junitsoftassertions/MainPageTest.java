a0bb7395915eb2eb483b07147a88842334d4db69
Eric Ricketts <eric_ricketts@me.com>
2023-04-23 06:08:55 -0400

    soft assertions in JUnit using AssertJ library

--- Section13/junit-soft-assertions/src/test/java/com/example/junitsoftassertions/MainPageTest.java
+++ Section13/junit-soft-assertions/src/test/java/com/example/junitsoftassertions/MainPageTest.java
@@ -1,15 +1,22 @@
 package com.example.junitsoftassertions;
 
+import org.assertj.core.api.SoftAssertions;
 import org.example.SetWebDriverLocation;
 import org.junit.jupiter.api.*;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -46,6 +53,15 @@ public class MainPageTest {
 
     @Test
     public void testUsingSoftAssertions() {
+        int expectedBrokenLinks = 1;
+        int expectedUnbrokenLinks = 19;
+        List<WebElement> unbrokenLinks = new ArrayList<>();
+        List<WebElement> brokenLinks = new ArrayList<>();
+
+        // create an instance of soft assertions
+        // These assertions will run all tests and not stop when there is a test failure
+        SoftAssertions softAssertions = new SoftAssertions();
+
         // scroll down to the footer and assert it is the viewport
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView(true)", mainPage.footerElement);
@@ -53,6 +69,31 @@ public class MainPageTest {
         boolean footerIsInView = wait.until(
                 ExpectedConditionUtils.isVisibleInViewport(mainPage.footerElement)
         );
-        Assertions.assertTrue(footerIsInView);
+        softAssertions.assertThat(footerIsInView).isTrue();
+
+        // cycle through all footer anchors check to see if they are valid
+        mainPage.footerAnchorElements.forEach(anchor -> {
+            String url = anchor.getAttribute("href");
+            try {
+                // to save time do not navigate to the website just grab the response header
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setRequestMethod("HEAD");
+                connection.connect();
+                // from the header obtain the response code
+                int responseCode = connection.getResponseCode();
+                if (responseCode >= 200 && responseCode < 400) {
+                    unbrokenLinks.add(anchor);
+                } else {
+                    brokenLinks.add(anchor);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        // make the assertions and then assert all
+        softAssertions.assertThat(brokenLinks.size()).isEqualTo(expectedBrokenLinks);
+        softAssertions.assertThat(unbrokenLinks.size()).isEqualTo(expectedUnbrokenLinks);
+        softAssertions.assertAll();
     }
 }
