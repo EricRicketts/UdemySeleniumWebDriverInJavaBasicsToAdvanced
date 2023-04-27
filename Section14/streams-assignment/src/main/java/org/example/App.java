b909dae00a627d85de3c3dcb4b6217343ad88850
Eric Ricketts <eric_ricketts@me.com>
2023-04-27 04:34:20 -0400

    finish working with streams

--- Section14/streams-assignment/src/main/java/org/example/App.java
+++ Section14/streams-assignment/src/main/java/org/example/App.java
@@ -1,14 +1,20 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class App {
     // this method takes a list of Integers and returns
     // a sorted list of unique numbers
-    public List<Integer> findUniqueIntegers(List<Integer> list) {
-        List<Integer> uniqueIntegerList =
+    public List<Integer> findUniqueIntegersAndSort(List<Integer> list) {
+        List<Integer> uniqueIntegerListSorted =
                 list.stream().distinct().sorted().toList();
-        return uniqueIntegerList;
+        return uniqueIntegerListSorted;
+    }
+    public List<Integer> findUniqueIntegersAndReverseSort(List<Integer> list) {
+        List<Integer> uniqueIntegerListReverseSorted =
+                list.stream().distinct().sorted(Comparator.reverseOrder()).toList();
+        return uniqueIntegerListReverseSorted;
     }
 }
--- Section14/streams-assignment/src/test/java/org/example/AppTest.java
+++ Section14/streams-assignment/src/test/java/org/example/AppTest.java
@@ -4,16 +4,35 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 
 public class AppTest {
 
     @Test
-    public void testApp() {
+    public void testUniqueAndSorted() {
         List<Integer> testList = Arrays.asList(3, 2, 2, 7, 5, 1, 9, 7);
         List<Integer> expected = Arrays.asList(1, 2, 3, 5, 7, 9);
-        List<Integer> results = new App().findUniqueIntegers(testList);
+        List<Integer> results = new App().findUniqueIntegersAndSort(testList);
+
+        Assertions.assertEquals(expected, results);
+    }
+
+    @Test
+    public void testUniqueAndReverseSorted() {
+        List<Integer> testList = Arrays.asList(3, 2, 2, 7, 5, 1, 9, 7);
+        List<Integer> expected = Arrays.asList(9, 7, 5, 3, 2, 1);
+        List<Integer> results = new App().findUniqueIntegersAndReverseSort(testList);
+
+        Assertions.assertEquals(expected, results);
+    }
+
+    @Test
+    public void testGetElementFromUniqueAndSorted() {
+        List<Integer> testList = Arrays.asList(3, 2, 2, 7, 5, 1, 9, 7);
+        int expected = 3;
+        int results = new App().findUniqueIntegersAndSort(testList).get(2);
 
         Assertions.assertEquals(expected, results);
     }
