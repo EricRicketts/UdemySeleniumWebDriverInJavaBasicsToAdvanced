1e800c6899c0b3c8608daf69a3c92b2dd9654373
Eric Ricketts <eric_ricketts@me.com>
2023-04-29 06:28:17 -0400

    assert on getting the fruit or vegetable prices from the web page

--- Section14/get-prices/src/test/java/com/example/getprices/MainPage.java
+++ Section14/get-prices/src/test/java/com/example/getprices/MainPage.java
@@ -6,10 +6,15 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 // page_url = https://www.jetbrains.com/
 public class MainPage {
     @FindBy(how = How.XPATH, using = "//thead/tr/th[1]")
     public WebElement fruitOrVegetableNameColumnHeading;
+
+    @FindBy(how = How.XPATH, using = "//tbody/tr")
+    public List<WebElement> fruitOrVegetableTableRows;
 
     public MainPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
--- Section14/get-prices/src/test/java/com/example/getprices/MainPageTest.java
+++ Section14/get-prices/src/test/java/com/example/getprices/MainPageTest.java
@@ -1,8 +1,7 @@
 package com.example.getprices;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,14 +12,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainPageTest {
     private WebDriver driver;
     private MainPage mainPage;
     WebDriverWait wait;
+    HashMap<String, Integer> fruitOrVegetableNamesAndPrices;
+    SoftAssertions softAssertions;
 
     @BeforeEach
     public void setUp() {
+        softAssertions = new SoftAssertions();
+        fruitOrVegetableNamesAndPrices = new HashMap<>();
         int implicitWaitTime = 5;
         int explicitWaitTime = 10;
         Duration duration = Duration.ofSeconds(explicitWaitTime);
@@ -43,10 +48,30 @@ public class MainPageTest {
 
     @Test
     public void testGetPrices() {
+        HashMap<String, Integer> expected = new HashMap<>();
+        expected.put("Wheat", 67);
+        expected.put("Tomato", 37);
+        expected.put("Strawberry", 23);
+        expected.put("Rice", 37);
+        expected.put("Potato", 34);
+
+        // wait until the table heading for the fruit or vegetable name is in view
+        // then assert it was in view
         boolean fruitOrVegetableNameColumnHeadingIsVisible = wait.until(
                 ExpectedConditions.textToBePresentInElement(
                         mainPage.fruitOrVegetableNameColumnHeading, "Veg/fruit name")
         );
-        assertTrue(fruitOrVegetableNameColumnHeadingIsVisible);
+        softAssertions.assertThat(fruitOrVegetableNameColumnHeadingIsVisible).isTrue();
+
+        // cycle through each of the rows and get the fruit or vegetable name and its corresponding price
+        mainPage.fruitOrVegetableTableRows.forEach(row -> {
+            String fruitOrVegetableName = row.findElement(By.cssSelector("td:first-child")).getText();
+            String fruitOrVegetablePriceText = row.findElement(By.cssSelector("td:nth-child(2)")).getText();
+            int fruitOrVegetablePrice = Integer.parseInt(fruitOrVegetablePriceText);
+
+            fruitOrVegetableNamesAndPrices.put(fruitOrVegetableName, fruitOrVegetablePrice);
+        });
+
+        softAssertions.assertThat(fruitOrVegetableNamesAndPrices).containsExactlyEntriesOf(expected);
     }
 }
