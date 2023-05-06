ba4cd977b485bca531c36c8e8694d2fd724ce262
Eric Ricketts <eric_ricketts@me.com>
2023-05-06 08:40:23 -0400

    finish multiple windows and tabs exercise

--- Section15/multiple-tabs-windows/src/test/java/com/example/multipletabswindows/MainPage.java
+++ Section15/multiple-tabs-windows/src/test/java/com/example/multipletabswindows/MainPage.java
@@ -16,6 +16,9 @@ public class MainPage {
   @FindBy(how = How.XPATH, using = "(//div[@id='courses-block']//a)[2]")
   public WebElement firstCourseLink;
 
+  @FindBy(how = How.CSS, using = "input.form-control[name$='name']")
+  public WebElement nameInput;
+
   public MainPage(WebDriver driver) {
     PageFactory.initElements(driver, this);
   }
--- Section15/multiple-tabs-windows/src/test/java/com/example/multipletabswindows/MainPageTest.java
+++ Section15/multiple-tabs-windows/src/test/java/com/example/multipletabswindows/MainPageTest.java
@@ -20,40 +20,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainPageTest {
-  private WebDriver driver;
-  private MainPage mainPage;
-  private WebDriverWait wait;
+    private WebDriver driver;
+    private MainPage mainPage;
+    private WebDriverWait wait;
 
-  @BeforeClass
-  public static void oneTimeSetup() {
-    SetWebDriverLocation.setDriverLocationAndDriverSystemProperty();
-  }
+    @BeforeClass
+    public static void oneTimeSetup() {
+        SetWebDriverLocation.setDriverLocationAndDriverSystemProperty();
+    }
 
-  @BeforeMethod
-  public void setUp() {
-    int implicitWaitTime = 5;
-    int explicitWaitTime = 10;
-    String url = "https://www.rahulshettyacademy.com/angularpractice/";
-    Duration duration = Duration.ofSeconds(explicitWaitTime);
-    ChromeOptions options = new ChromeOptions();
-    // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
-    options.addArguments("--remote-allow-origins=*");
-    driver = new ChromeDriver(options);
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitTime));
-    driver.get(url);
+    @BeforeMethod
+    public void setUp() {
+        int implicitWaitTime = 5;
+        int explicitWaitTime = 10;
+        String url = "https://www.rahulshettyacademy.com/angularpractice/";
+        Duration duration = Duration.ofSeconds(explicitWaitTime);
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitTime));
+        driver.get(url);
 
-    mainPage = new MainPage(driver);
-    wait = new WebDriverWait(driver, duration);
-  }
+        mainPage = new MainPage(driver);
+        wait = new WebDriverWait(driver, duration);
+    }
 
-  @AfterMethod
-  public void tearDown() {
-    driver.quit();
-  }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 
-  @Test
-  public void testMultipleWindowsAndTabs() throws InterruptedException {
+    @Test
+    public void testMultipleWindowsAndTabs() {
     /*
       In this test we navigate to Rahul Shetty's angular page and are required
       to enter the first course name of his courses offered page which is at
@@ -61,36 +61,49 @@ public class MainPageTest {
       to grab the data and then navigate back to the original url to enter the
       data.
     */
-    String secondUrl = "https://rahulshettyacademy.com/";
-    // wait for the footer element to appear
-    WebElement mainPageFooter = wait.until(
-        ExpectedConditions.visibilityOf(mainPage.mainPageFooter)
-    );
-    Assert.assertNotNull(mainPageFooter);
+        String secondUrl = "https://rahulshettyacademy.com/";
+        // wait for the footer element to appear
+        WebElement mainPageFooter = wait.until(
+                ExpectedConditions.visibilityOf(mainPage.mainPageFooter)
+        );
+        Assert.assertNotNull(mainPageFooter);
 
-    // create a new tab and navigate to the desired url
-    // open a blank tab
-    driver.switchTo().newWindow(WindowType.TAB);
+        // create a new tab and navigate to the desired url
+        // open a blank tab
+        driver.switchTo().newWindow(WindowType.TAB);
 
-    // get all the existing window handles then chose the new tab window handle
-    List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
-    String originalWindowHandle = windowHandles.get(0);
-    String newTabHandle = windowHandles.get(1);
-    driver.switchTo().window(newTabHandle);
-    // load the desired url
-    driver.get(secondUrl);
+        // get all the existing window handles then chose the new tab window handle
+        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
+        String originalWindowHandle = windowHandles.get(0);
+        String newTabHandle = windowHandles.get(1);
+        driver.switchTo().window(newTabHandle);
+        // load the desired url
+        driver.get(secondUrl);
 
-    // verify landing on the new tab with its url
-    WebElement newTabFooter = wait.until(
-        ExpectedConditions.visibilityOf(mainPage.newTabFooter)
-    );
-    Assert.assertNotNull(newTabFooter);
+        // verify landing on the new tab with its url
+        WebElement newTabFooter = wait.until(
+                ExpectedConditions.visibilityOf(mainPage.newTabFooter)
+        );
+        Assert.assertNotNull(newTabFooter);
 
-    // get the text for the first course
-    Assert.assertNotNull(mainPage.firstCourseLink);
-    String firstCourseTextDescription = mainPage.firstCourseLink.getText();
+        // get the text for the first course
+        Assert.assertNotNull(mainPage.firstCourseLink);
+        String firstCourseTextDescription = mainPage.firstCourseLink.getText();
 
-    driver.switchTo().window(originalWindowHandle);
-//    Thread.sleep(2000);
-  }
+        // now that the desired input value is in a string variable
+        // we need to put it in the input field of the original window
+
+        // switch back to the original window and verify we can see the input
+        driver.switchTo().window(originalWindowHandle);
+        WebElement nameInput = wait.until(
+                ExpectedConditions.visibilityOf(mainPage.nameInput)
+        );
+        Assert.assertNotNull(nameInput);
+
+        // type in the desired name in the input field and
+        // verify the input field has capture it
+        nameInput.sendKeys(firstCourseTextDescription);
+        Assert.assertEquals(nameInput.getAttribute("value"),
+                firstCourseTextDescription);
+    }
 }
