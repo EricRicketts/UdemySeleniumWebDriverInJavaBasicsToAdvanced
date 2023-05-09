c718b90e0c3f3c76f50c3932e347a26f28ca82a5
Eric Ricketts <eric_ricketts@me.com>
2023-05-09 05:29:58 -0400

    I might actually have a testng test running properly

---
+++ Section16/testng-examples/src/main/java/org/example/Person.java
@@ -0,0 +1,53 @@
+package org.example;
+
+public class Person {
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public Person() {}
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+}
---
+++ Section16/testng-examples/src/test/java/org/example/AppTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class AppTest {
+
+    private Person firstPerson, secondPerson;
+    @BeforeClass
+    public static void oneTimeSetup() {
+        SetWebDriverLocation.setDriverLocationAndDriverSystemProperty();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+       firstPerson = new Person();
+       secondPerson = new Person("Elmer", "Fudd", 35);
+    }
+
+    @Test
+    public void testFirstPersonAge() {
+        Assert.assertEquals(firstPerson.getAge(), 0);
+    }
+
+    @Test
+    public void testFirstPersonSetAge() {
+        int age = 30;
+        firstPerson.setAge(age);
+        Assert.assertEquals(firstPerson.getAge(), age);
+    }
+
+    @Test
+    public void testFirstPersonSetFirstName() {
+        String firstName = "Daffy";
+        firstPerson.setFirstName(firstName);
+        Assert.assertEquals(firstPerson.getFirstName(), firstName);
+    }
+
+    @Test
+    public void testFirstPersonSetLastName() {
+        String lastName = "Duck";
+        firstPerson.setLastName(lastName);
+        Assert.assertEquals(firstPerson.getLastName(), lastName);
+    }
+
+}
