34126a1ec039f77004f7c8b0ceff0c3faa4770c5
Eric Ricketts <eric_ricketts@me.com>
2023-06-09 17:55:47 -0400

    define all tests for testng instruction

---
+++ Section16/testng-examples/src/main/java/org/example/Automobile.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Automobile {
+
+    private String make;
+    private String model;
+    private int year;
+    private BigDecimal price;
+
+    public Automobile(
+            String make, String model, int year, BigDecimal price
+    ) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+}
