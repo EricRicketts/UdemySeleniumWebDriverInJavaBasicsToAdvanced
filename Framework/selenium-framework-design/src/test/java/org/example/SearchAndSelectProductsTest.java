f4bd4063ed4380fc39326552d31d3e9fd905a5a6
Eric Ricketts <eric_ricketts@me.com>
2023-06-21 19:41:31 -0400

    arrive on home page for the shopping application

---
+++ Framework/selenium-framework-design/src/test/java/org/example/SearchAndSelectProductsTest.java
@@ -0,0 +1,62 @@
+package org.example;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SearchAndSelectProductsTest {
+
+    final String userEmail = "elmer.fudd@warnerbros.com";
+    final String userPassword = "Bugs123@bunny";
+    private WebDriver driver;
+    private WebDriverWait wait;
+    @BeforeMethod
+    public void setUp() {
+        Duration duration;
+        int implicitWaitTime = 5;
+        int explicitWaitTime = 10;
+        WebDriverManager.chromedriver().setup();
+        String URL = "https://rahulshettyacademy.com/client";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitTime));
+
+        duration = Duration.ofSeconds(explicitWaitTime);
+        wait = new WebDriverWait(driver, duration);
+        driver.get(URL);
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testSelectAndBuyProducts() {
+        driver.findElement(By.id("userEmail")).sendKeys(userEmail);
+        driver.findElement(By.id("userPassword")).sendKeys(userPassword);
+        driver.findElement(By.id("login")).click();
+
+        WebElement headingForPageTitle = driver.findElement(By.cssSelector("div.mt-1 > h3"));
+        Boolean pageHeadingMainTextFound = wait.until(
+                ExpectedConditions.textToBePresentInElement(headingForPageTitle, "AUTOMATION")
+        );
+
+        Assert.assertTrue(pageHeadingMainTextFound);
+    }
+}
