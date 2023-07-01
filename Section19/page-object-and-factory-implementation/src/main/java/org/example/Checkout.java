87b9a2734b15076af5c3f49367b812dfd30944b3
Eric Ricketts <eric_ricketts@me.com>
2023-07-01 13:16:59 -0400

    finish defining all element locators for checkout page

--- Section19/page-object-and-factory-implementation/src/main/java/org/example/Checkout.java
+++ Section19/page-object-and-factory-implementation/src/main/java/org/example/Checkout.java
@@ -8,6 +8,12 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Checkout {
 
+    @FindBy(how = How.XPATH, using = "//button[contains(text(), 'Apply Coupon')]")
+    public WebElement getApplyCouponButton;
+    
+    @FindBy(how = How.XPATH, using = "//div[contains(text(), 'Apply Coupon')]/following-sibling::input")
+    public WebElement applyCouponInput;
+
     @FindBy(how = How.CSS, using = "div.user__address  input")
     public WebElement country;
 
@@ -17,8 +23,20 @@ public class Checkout {
     @FindBy(how = How.CSS, using = "div.payment__types div:nth-of-type(1)")
     public WebElement creditCardPaymentMethod;
 
+    @FindBy(how = How.XPATH, using = "//div[contains(text(), 'CVV Code')]/following-sibling::input")
+    public WebElement cvvInput;
+
+    @FindBy(how = How.XPATH, using = "//div[contains(text(), 'Name on Card')]/following-sibling::input")
+    public WebElement nameOnCardInput;
+
     @FindBy(how = How.XPATH, using = "//a[contains(text(), 'Place Order')]")
     public WebElement placeOrderButton;
+
+    @FindBy(how = How.CSS, using = "select:nth-of-type(1)")
+    public WebElement selectExpirationMonth;
+
+    @FindBy(how = How.CSS, using = "select:nth-of-type(2)")
+    public WebElement selectExpirationYear;
 
     @FindBy(how = How.CSS, using = "div.user__name > input")
     public WebElement usernameInput;
